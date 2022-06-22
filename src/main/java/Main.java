c7146b8e7558977c0d20255403aa0205911a1aed
davronismoilov <ismoilovdavron16@gmail.com>
2022-06-22 09:09:44 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("dependency");
+    }
+
+    public  static  void
+    test(){
+        System.out.println(
+            "vhbjacnls;fjehbjlnks;alfvbnksa"
+        );
+    }
+}
